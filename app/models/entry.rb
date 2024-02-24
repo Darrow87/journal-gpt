fc80ee00f2c01da3e2371b3a0e295c794ab649c3
Alex Forger <alexforgerii@gmail.com>
2024-02-24 06:28:29 -1000

    saved filed to add validations etc

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,9 @@
 class Entry < ApplicationRecord
+  # Association
   belongs_to :user
+  
+  # Validations
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :date, presence: true
 end
