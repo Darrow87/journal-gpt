ee9e9d894248598191a16073c203153204d5b734
Alex Forger <alexforgerii@gmail.com>
2024-02-24 17:27:35 -1000

    change entry model because I wont be storing this info in the db and make it usable for an api call

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,27 @@
-class Entry < ApplicationRecord
-  # Association
-  belongs_to :user
-  
-  # Validations
+class Entry
+  include ActiveModel::Model
+  include ActiveModel::Validations
+
+  attr_accessor :title, :body, :date, :user
+
   validates :title, presence: true
   validates :body, presence: true
   validates :date, presence: true
+
+  def initialize(attributes = {})
+    @title = attributes[:title]
+    @body = attributes[:body]
+    @date = attributes[:date]
+    @user = attributes[:user]
+  end
+
+  def submit
+    if valid?
+      # Here you would make the API call to ChatGPT with the entry data
+      # For example: ChatGPTService.generate_response(self)
+      true
+    else
+      false
+    end
+  end
 end
